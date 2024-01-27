a9d35ab53f0a2bfb03e7e55c35041f811114477b
imranulmulk <imranulmulk1@gmail.com>
2024-01-27 18:27:06 +0500

    i-commit stories completed

--- src/components/contents/Pdfs/Stories/Stories.js
+++ src/components/contents/Pdfs/Stories/Stories.js
@@ -1,10 +1,12 @@
 import {View, Text, FlatList, Image, Pressable} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {firebase} from '../../../../../firebase/config';
+import Loader from '../../../../layouts/Loader';
 
 export default function Stories({navigation}) {
   const [storiesData, setStoriesData] = useState([]);
   const storiesRef = firebase.firestore().collection('stories');
+  const [loading, setLoading] = useState(true); //for loader
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +24,7 @@ export default function Stories({navigation}) {
         });
         // Set the fetched stories in the state
         setStoriesData(stories);
-        console.log(stories); // Log the fetched data
+        setLoading(false); // when data is fetched set loading to false
       } catch (error) {
         console.error('Error fetching data: ', error);
       }
@@ -33,50 +35,56 @@ export default function Stories({navigation}) {
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <FlatList
-        contentContainerStyle={{
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        showsVerticalScrollIndicator={false}
-        data={storiesData}
-        numColumns={2}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => (
-          <Pressable
-            onPress={() => navigation.navigate('ViewStory', {storyData: item})}
-            style={{
-              width: 140,
-              height: 250,
-              borderWidth: 1,
-              borderColor: '#9fe0fc',
-              borderRadius: 10,
-              margin: 20,
-              backgroundColor: '#D1F1FF',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Image
+      {loading ? (
+        <Loader loadingText="Loading Stories..." />
+      ) : (
+        <FlatList
+          contentContainerStyle={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          showsVerticalScrollIndicator={false}
+          data={storiesData}
+          numColumns={2}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Pressable
+              onPress={() =>
+                navigation.navigate('ViewStory', {storyData: item})
+              }
               style={{
-                // borderRadius: 50,
-                width: '100%',
-                height: 170,
-                objectFit: 'contain',
-              }}
-              source={{uri: item.imgUrl}}
-            />
-            <Text
-              style={{
-                marginTop: 20,
-                fontWeight: 'bold',
-                color: 'black',
-                textAlign: 'center',
+                width: 140,
+                height: 250,
+                borderWidth: 1,
+                borderColor: '#9fe0fc',
+                borderRadius: 10,
+                margin: 20,
+                backgroundColor: '#D1F1FF',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
-              {item.name}
-            </Text>
-          </Pressable>
-        )}
-      />
+              <Image
+                style={{
+                  // borderRadius: 50,
+                  width: '100%',
+                  height: 170,
+                  objectFit: 'contain',
+                }}
+                source={{uri: item.imgUrl}}
+              />
+              <Text
+                style={{
+                  marginTop: 20,
+                  fontWeight: 'bold',
+                  color: 'black',
+                  textAlign: 'center',
+                }}>
+                {item.name}
+              </Text>
+            </Pressable>
+          )}
+        />
+      )}
     </View>
   );
 }
