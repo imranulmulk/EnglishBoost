bb33f45b5350626f12468b6dfdb5d0de4a7db9fb
imranulmulk <imranulmulk1@gmail.com>
2024-02-07 02:32:59 +0500

    i-commit recommended books added

---
+++ src/components/contents/Pdfs/Books/RecommendedBooks.js
@@ -0,0 +1,126 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, Image, Pressable} from 'react-native';
+import {firebase} from '../../../../../firebase/config';
+import Loader from '../../../../layouts/Loader';
+import {useNavigation} from '@react-navigation/native';
+
+const RecommendedBooks = () => {
+  const [recommendedBooksData, setRecommendedBooksData] = useState([]);
+  const recommendedBooksRef = firebase.firestore().collection('books');
+  const [loading, setLoading] = useState(true); // for loader
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchRecommendedBooks = async () => {
+      try {
+        const querySnapshot = await recommendedBooksRef.limit(4).get();
+        const recommendedBooks = querySnapshot.docs.map(doc => {
+          const {key, name, imgUrl, pdfUrl} = doc.data();
+          return {
+            id: doc.id,
+            key,
+            name,
+            imgUrl,
+            pdfUrl,
+          };
+        });
+        const booksWithButton = [
+          ...recommendedBooks,
+          {
+            token: false,
+            // source: require('../../../../../assets/more.png'),
+            name: 'See All',
+          },
+        ];
+        // Set the fetched recommended books in the state
+        setRecommendedBooksData(booksWithButton);
+        setLoading(false); // when data is fetched set loading to false
+      } catch (error) {
+        console.error('Error fetching recommended books data: ', error);
+      }
+    };
+
+    fetchRecommendedBooks();
+  }, []);
+
+  const handlePress = item => {
+    if (item.name === 'See All') {
+      // Redirect to the 'Books' component
+      navigation.navigate('Books');
+    } else {
+      // Redirect to 'ViewBook' for other items
+      navigation.navigate('ViewBook', {bookData: item});
+    }
+  };
+
+  return (
+    <View style={{flex: 1, margin: 15}}>
+      <Text style={{fontSize: 22, fontWeight: 'bold', color: '#000'}}>
+        Recommended Books
+      </Text>
+      {loading ? (
+        <Loader loadingText="Loading Recommended Books..." />
+      ) : (
+        <FlatList
+          contentContainerStyle={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={recommendedBooksData}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Pressable
+              onPress={() => handlePress(item)}
+              style={{
+                width: 100,
+                height: 170,
+                borderWidth: 1,
+                borderColor: '#9fe0fc',
+                borderRadius: 10,
+                margin: 10,
+                backgroundColor: '#D1F1FF',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              {item.name === 'See All' ? (
+                <Image
+                  style={{
+                    width: '60%',
+                    height: 90,
+                    resizeMode: 'contain',
+                  }}
+                  source={require('../../../../../assets/more.png')}
+                />
+              ) : (
+                <Image
+                  style={{
+                    width: '100%',
+                    height: 100,
+                    resizeMode: 'contain',
+                  }}
+                  source={{uri: item.imgUrl}}
+                />
+              )}
+              <Text
+                style={{
+                  marginTop: 5,
+                  fontWeight: 'bold',
+                  color: '#004aad',
+                  textAlign: 'center',
+                  fontSize: 12,
+                }}
+                numberOfLines={2}
+                ellipsizeMode="tail">
+                {item.name}
+              </Text>
+            </Pressable>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+export default RecommendedBooks;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,17 @@
-import {View, Text, SafeAreaView, Pressable, StatusBar} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Pressable,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Videos from '../components/contents/AllVideos/Videos';
 import {useNavigation} from '@react-navigation/native';
 import styles from '../../Styles/WordofTheDay';
+import RecommendedBooks from '../components/contents/Pdfs/Books/RecommendedBooks';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,29 +22,34 @@ const HomeScreen = () => {
         // justifyContent: 'center',
         backgroundColor: '#fff',
       }}>
-      <StatusBar backgroundColor="#0079FF" />
-      {/* Navbar Section */}
-      <View>
-        <Navbar />
-      </View>
-      {/* Word of the day section */}
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingTop: 10,
-          padding: 10,
-        }}>
-        <Pressable
-          style={styles.wordofday}
-          onPress={() => navigation.navigate('WordOfTheDay')}>
-          <Text style={styles.wText}>Word Of the Day</Text>
-        </Pressable>
-      </View>
-      {/* Videos Section */}
-      <View style={{height: '46%'}}>
-        <Videos />
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <StatusBar backgroundColor="#0079FF" />
+        {/* Navbar Section */}
+        <View>
+          <Navbar />
+        </View>
+        {/* Word of the day section */}
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: 10,
+            padding: 10,
+          }}>
+          <Pressable
+            style={styles.wordofday}
+            onPress={() => navigation.navigate('WordOfTheDay')}>
+            <Text style={styles.wText}>Word Of the Day</Text>
+          </Pressable>
+        </View>
+        <View style={{backgroundColor: 'red'}}>
+          <RecommendedBooks />
+        </View>
+        {/* Videos Section */}
+        <View style={{backgroundColor: 'yellow'}}>
+          <Videos />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
